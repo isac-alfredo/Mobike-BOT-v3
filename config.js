d2bfe9d0a72dddbbeb8f3ef3a4962e13a60dddff
Isac Alfredo <82800581+isac-alfredo@users.noreply.github.com>
2026-08-19 11:35:56 +0200

    Atualizar o config.js

--- config.js
+++ config.js
@@ -3,8 +3,7 @@ module.exports = {
     ownerName: 'Isac Alfredo',           // Seu nome
     prefix: '/',
     botName: 'MOBIKE-BOT',
-   mongoURI: process.env.MONGO_URI || 'mongodb+srv://boyisact_db_user:dMqvuS6J21kciNOn@cluster0.68e8bi3.mongodb.net/?appName=Cluster0',
-
+   
     // Suas redes sociais
     telegram: 'https://t.me/isacalfredo39',
     youtube: 'https://youtube.com/@mobikemoz6407?si=k4DADj67tuqyWYdH',
